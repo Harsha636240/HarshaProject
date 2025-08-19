d342a22ae4a86416a877029111c948e06c92a212
Harsha636240 <harsha636240@gmail.com>
2025-08-19 23:47:12 +0530

    Add Integer in linkList and print it.

--- linkedList.java
+++ linkedList.java
@@ -1,6 +1,17 @@
+
+import java.util.*;
 public class linkedList {
 
 public static void main(String []args){
-    System.out.println("Hello World");
+
+    LinkedList <Integer> List = new LinkedList<>();
+
+    List.add(1);
+    List.add(2);
+    List.add(3);
+    List.add(1,4);
+
+    System.out.println(List);
+
 }
 }
